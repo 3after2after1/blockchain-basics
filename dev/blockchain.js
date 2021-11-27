104ed846443ad4ca79f8b1cf102d59f223360362
Balaganabathy Krishnanmoorthy <gana.ganabathy@gmail.com>
2021-11-27 13:11:17 +0800

    section 2 done

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,30 +1,77 @@
+import crypto from "crypto";
+
 class Blockchain {
   constructor() {
     this.chain = [];
-    this.newTransactions = [];
-    //? hold all of new transactions created before they are placed into a block
+    this.pendingTransactions = [];
+    //* hold all of new transactions created before they are placed into a block
+    //* on our blockchain, there going to be a lot of people who make different transactions
+    //* these transactions are not recorded into our blockchain yet
+    //* they get recored when a new block is created/mined
+    //* hence all of these transactions are pending and not validated yet
+    this.createNewBlock(100, "0", "0");
   }
 
   createNewBlock(nonce, previousBlockHash, hash) {
     const newBlock = {
       index: this.chain.length + 1,
       timeStamp: Date.now(),
-      transactions: this.newTransactions,
-      //?all of the transactions in this block should be new transactions waiting to be placed into a block
+      transactions: this.pendingTransactions,
+      //*all of the transactions in this block should be pending transactions waiting to be placed into a block
       nonce: nonce,
-      //? can be any number, basically a proof that we created this new block in a legitimate way using POW method
+      //* can be any number, basically a proof that we created this new block in a legitimate way using POW method
       hash: hash,
-      //? this hash will be the data from our new block
-      //? We will pass all our new transactions into hashing function [will be compressed into single string of code]
+      //* this hash will be the data from our new block
+      //* We will pass all our new transactions into hashing function [will be compressed into single string of code]
       previousBlockHash: previousBlockHash,
     };
 
-    this.newTransactions = [];
-    //?after new block created, we put all new transactions since last block was mined, into this new block
-    //?so we want to clear this entire transactions array so that we can start over for the next block
+    this.pendingTransactions = [];
+    //* after new block created, we put all new transactions since last block was mined, into this new block
+    //* so we want to clear this entire transactions array so that we can start over for the next block
     this.chain.push(newBlock);
 
     return newBlock;
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  createNewTransaction(amount, sender, recipient) {
+    const newTransaction = {
+      amount: amount,
+      sender: sender,
+      recipient: recipient,
+    };
+    //* all transactions in our blockchain will look like this
+    this.pendingTransactions.push(newTransaction);
+    return this.getLastBlock()["index"] + 1;
+    //* what block we will be able to find this new transaction in
+    //* will be in the block mined/created next
+  }
+
+  hashBlock(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString =
+      previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = crypto.createHash("sha256").update(dataAsString).digest("hex");
+    return hash;
+    //* take a block from our blockchain and hash that block into some fixed length string
+    //* with any given input, the output hash will alwways be the same for that particular input
+    //* currentBlockData is an array containing objects that represent transactions for current block
+  }
+
+  checkValid(previousBlockHash, currentBlockData) {
+    //* repeatedly hash block until it finds correct hash starting with four zeros '0000AISDPD...'
+    //* continuously change nonce value until it finds correct hash
+    //* returns to us the nonce value that creates correct hash
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0, 4) !== "0000") {
+      nonce = nonce + 1;
+      hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
   }
 }
 
--- dev/main.js
+++ dev/main.js
@@ -2,6 +2,25 @@ import Blockchain from "./blockchain.js";
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, "dwd242424", "fgdfg54t43");
+const previousBlockHash = "ADON3O48G539HG3FF3NEFU";
 
-console.log(bitcoin);
+const currentBlockData = [
+  {
+    amount: 101,
+    sender: "ADAKNDLN48T4NT8N43",
+    recipient: "KNSDS8FHSF8S8FS",
+  },
+  {
+    amount: 30,
+    sender: "MCOEMVRE9VJJ3V3RJ",
+    recipient: "MC9SUD8SDS7D9SH",
+  },
+  {
+    amount: 200,
+    sender: "SDMS877YFD7HF7DYF",
+    recipient: "38392R8EF9DFNIF",
+  },
+];
+
+// console.log(bitcoin.checkValid(previousBlockHash, currentBlockData));
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 335141));
